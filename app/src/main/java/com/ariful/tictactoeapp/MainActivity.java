0b32aafbafa0ed2ca916982c76a2f201863f939e
chowdhuryarif17@gmail.com <chowdhuryarif17@gmail.com>
2020-11-30 19:44:45 +0600

    little changed befoe leaving office

--- app/src/main/java/com/ariful/tictactoeapp/MainActivity.java
+++ app/src/main/java/com/ariful/tictactoeapp/MainActivity.java
@@ -3,13 +3,17 @@ package com.ariful.tictactoeapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView playerOneScore, playerTwoScore, playerStatus;
+    private Button [] buttons = new Button[9];
+    private Button resetGame;
 
-
+    private int playerOneScoreCount, playerTwoScoreCount, rountCount;
+    boolean activePlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
